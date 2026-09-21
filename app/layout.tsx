1cea36ea40301ac1bbaee3c659318d9cd05f654f
nualdaber <nurss.aldb@gmail.com>
2026-09-21 23:48:26 +0500

    Use JUZ40 Inter typography sitewide

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { headers } from "next/headers";
-import "@fontsource-variable/noto-sans";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
 import "./globals.css";
 
 export async function generateMetadata(): Promise<Metadata> {
